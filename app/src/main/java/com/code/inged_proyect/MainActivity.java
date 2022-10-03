d5d7a6163dbea8f85babeb1980979b91e1e0bb76
WilliamRVazquez <wvazquez0@ucol.mx>
2022-10-03 11:30:18 -0500

    Cambio de activity usando texto

--- app/src/main/java/com/code/inged_proyect/MainActivity.java
+++ app/src/main/java/com/code/inged_proyect/MainActivity.java
@@ -2,13 +2,25 @@ package com.code.inged_proyect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtRegister = (TextView) findViewById(R.id.txtRegister);
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = new Intent(MainActivity.this, Sing_up.class);
+                startActivity(i);
+            }
+        });
     }
 }
